3d6f1904f7b59b5d251a741faab3c10842a2ca2c
JoshuaBentley <joshuabentley911@gmail.com>
2023-01-25 11:16:11 -0700

    created code to write html file

--- __test__/employee.test.js
+++ __test__/employee.test.js
@@ -5,14 +5,42 @@ describe('Employee', () => {
         it('should create a new isntance of employee with name, ID, and position classes', () => {
             const name = "Luffy"
             const id = 14
-            const position = "Engineer" 
+            const position = "Manager" 
 
             const newEmployee = new Employee(name, id, position)
 
             expect(newEmployee instanceof Employee).toBe(true)
             expect(newEmployee.name).toEqual('Luffy')
             expect(newEmployee.id).toEqual(14)
-            expect(newEmployee.position).toEqual('Engineer')
+            expect(newEmployee.position).toEqual('Manager')
+        })
+    })
+
+    describe('getPosition', () => {
+        it('should return the position of the employee', () => {
+        const name = "Luffy"
+        const id = 14
+        const position = "Manager" 
+
+        const newEmployee = new Employee(name, id, position)
+
+        const result = newEmployee.getPosition()
+
+        expect(result).toEqual('Manager')
+        })
+    })
+
+    describe('getName', () => {
+        it('should return the name of the employee', () => {
+        const name = "Luffy"
+        const id = 14
+        const position = "Manager" 
+
+        const newEmployee = new Employee(name, id, position)
+
+        const result = newEmployee.getName()
+
+        expect(result).toEqual('Luffy')
         })
     })
 })
---
+++ src/createEmployee.js
@@ -0,0 +1,109 @@
+const createEmployee = (employee) => {
+    const addManager = (manager) => {
+        return `
+        <div class="col d-flex justify-content-center">
+        <div class="card" style="width: 18rem">
+          <div class="card-body">
+            <ul class="list-group">
+              <li class="list-group-item heading">${manager.getName()}</li>
+              <li class="list-group-item heading">
+                <i class="fa-solid fa-user"></i>${manager.getPosition()}
+              </li>
+              <li class="list-group-item">Naruto</li>
+              <li class="list-group-item">Asta</li>
+            </ul>
+          </div>
+        </div>
+        `
+    }
+
+    const addEngineer = (engineer) => {
+        return `
+        <div class="col d-flex justify-content-center">
+        <div class="card" style="width: 18rem">
+          <div class="card-body">
+            <ul class="list-group">
+              <li class="list-group-item heading">${engineer.getName()}</li>
+              <li class="list-group-item heading">
+                <i class="fa-solid fa-user"></i>${engineer.getPosition()}
+              </li>
+              <li class="list-group-item">Naruto</li>
+              <li class="list-group-item">Asta</li>
+            </ul>
+          </div>
+        </div>
+        `
+    }; 
+
+    const addIntern = (engineer) => {
+      return `
+      <div class="col d-flex justify-content-center">
+      <div class="card" style="width: 18rem">
+        <div class="card-body">
+          <ul class="list-group">
+            <li class="list-group-item heading">${engineer.getName()}</li>
+            <li class="list-group-item heading">
+              <i class="fa-solid fa-user"></i>${engineer.getPosition()}
+            </li>
+            <li class="list-group-item">Naruto</li>
+            <li class="list-group-item">Asta</li>
+          </ul>
+        </div>
+      </div>
+      `
+  }; 
+    const html = [];
+
+    html.push(
+         employee
+            .filter((allEmployees) => allEmployees.getPosition() === 'Manager')
+            .map((manager) => addManager(manager))
+            .join('')
+    );
+    html.push(
+      employee
+         .filter((allEmployees) => allEmployees.getPosition() === 'Engineer')
+         .map((engineer) => addEngineer(engineer))
+         .join('')
+    );
+    html.push(
+      employee
+         .filter((allEmployees) => allEmployees.getPosition() === 'Intern')
+         .map((intern) => addIntern(intern))
+         .join('')
+    );  
+    return html.join('')
+}    
+
+module.exports = (employee) => {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+          crossorigin="anonymous"
+        />
+        <link rel="stylesheet" href="./styles/reset.css" />
+        <link rel="stylesheet" href="./styles/css/fontawesome.min.css" />
+        <link rel="stylesheet" href="./styles/css/solid.min.css" />
+        <link rel="stylesheet" href="./styles/css/brands.min.css" />
+        <link rel="stylesheet" href="./styles/style.css" />
+        <title>Employee thingy</title>
+      </head>
+      <body>
+        <h1>Employee thingy</h1>
+        <div class="container text-center">
+          <div class="row">
+            ${createEMployee(employee)}
+          </div>
+        </div>
+      </body>
+    </html>
+    `
+};
